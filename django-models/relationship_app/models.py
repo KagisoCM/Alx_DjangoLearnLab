66a922db024ec4725c8c1d3facbd1f2b972fd8e5
MohammadNyundo <mohammadnyundo00122@gmail.com>
2025-08-01 01:43:38 +0300

    Refactor project structure and update settings/urls

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
@@ -7,15 +8,25 @@ class Author(models.Model):
         return self.name
 
 class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey('Author', on_delete=models.CASCADE)
+    library = models.ForeignKey('Library', on_delete=models.CASCADE, related_name='books')
+
+    class Meta:
+        permissions = [
+            ('can_add_book', 'Can add a book'),
+            ('can_change_book', 'Can change a book'),
+            ('can_delete_book', 'Can delete a book'),
+        ]
 
     def __str__(self):
         return self.title
 
+
+
 class Library(models.Model):
     name = models.CharField(max_length=100)
-    books = models.ManyToManyField(Book)
+    # books = models.ManyToManyField(Book)
 
     def __str__(self):
         return self.name
@@ -26,3 +37,17 @@ class Librarian(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class UserProfile(models.Model):
+    ROLE_CHOICES = (
+        ('Admin', 'Admin'),
+        ('Librarian', 'Librarian'),
+        ('Member', 'Member'),
+    )
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.role}"
--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,3 +1,18 @@
 from django.urls import path
+from . import views
+from .views import add_book, edit_book, delete_book
+urlpatterns = [
+    path('books/', views.list_books, name='list_books'),
+    path('libraries/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 
-urlpatterns = []
+    # Auth URLs
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('register/', views.register_view, name='register'),
+    path('admin-view/', views.admin_view, name='admin_view'),
+    path('librarian-view/', views.librarian_view, name='librarian_view'),
+    path('member-view/', views.member_view, name='member_view'),
+    path('books/add/', add_book, name='add_book'),
+    path('books/edit/<int:pk>/', edit_book, name='edit_book'),
+    path('books/delete/<int:pk>/', delete_book, name='delete_book'),
+]
