1eaa4813e79534fd0d9bd23660f239b635447aef
MohammadNyundo <mohammadnyundo00122@gmail.com>
2025-07-31 23:58:06 +0300

    Initial commit with advanced model relationships and sample queries

---
+++ relationship_app/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Author
+from .forms import AuthorForm
+
+def author_list(request):
+    authors = Author.objects.all()
+    return render(request, 'relationship_app/author_list.html', {'authors': authors})
+
+def author_create(request):
+    if request.method == 'POST':
+        form = AuthorForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('author_list')
+    else:
+        form = AuthorForm()
+    return render(request, 'relationship_app/author_form.html', {'form': form})
+
+def author_edit(request, pk):
+    author = get_object_or_404(Author, pk=pk)
+    if request.method == 'POST':
+        form = AuthorForm(request.POST, instance=author)
+        if form.is_valid():
+            form.save()
+            return redirect('author_list')
+    else:
+        form = AuthorForm(instance=author)
+    return render(request, 'relationship_app/author_form.html', {'form': form})
+
+def author_delete(request, pk):
+    author = get_object_or_404(Author, pk=pk)
+    if request.method == 'POST':
+        author.delete()
+        return redirect('author_list')
+    return render(request, 'relationship_app/author_confirm_delete.html', {'author': author})
